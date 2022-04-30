294b81cfec22778acf3fed34acfcffcfcf9ff5dc
ulisMac <rosas_u@ymail.com>
2022-04-30 13:35:41 -0700

    added dependencises to manifest and aded two files for offline usage

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+// declare variales. Example: StuffToStore  
+// this will cache the information that we need off line 
+
+
+
+// install serviceworker
+
+
+// fetch the service worker
+
+// activate the ervice worker
