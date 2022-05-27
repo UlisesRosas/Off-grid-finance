4612d51cc4ca900662f222fd2ec287efbab6ce25
ulisMac <rosas_u@ymail.com>
2022-05-27 03:05:49 -0700

    created a sservice worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,69 @@
-// declare variables. Example: StuffToStore  
-// this will cache the information that we need off line 
-const STUFF_TO_STORE = [
-    "./index.html",
-    "./css/styles.css",
-    "./"
-]
+const APP_PREFIX = 'Off-grid-finance-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
+];
 
 
-
-// install serviceworker
-self.addEventListener('install', function (e) {
-
+// respond with cached resources 
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            // if cache is available, respond with cache
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request;
+            }
+            // if there is no cache available, try fetch request 
+            else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request);
+            }
+        })
+    )
 })
 
-// fetch the service worker
+// Cache resources 
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
 
-// activate the service worker
+// Delete outdated caches 
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            // `keyList` contains all cache names under your username.github.io. Filter out ones that has this app prefix to create keeplist
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            // add current cache name to keeplist
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
+});
