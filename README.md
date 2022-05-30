8a1ad1b29c5f962d80e4f2a638143a5dd69a9361
ulisMac <rosas_u@ymail.com>
2022-05-30 01:54:29 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-## An app that can track a budget even when you are off line and off the grid
+ # Application
+Off Grid Finance
 
-# Application
-[deployed application] (https://agile-gorge-94618.herokuapp.com/)
+
+## **Description**
+An app that can track a budget weather you are connected or on the go. Off Grid Finance will reliably work in diverse situations.
+
+## Deployed Application
+[Visit App Here](./public/assets/images/App-Screen-Shot.png)
+
+![APP](./public/assets/images/App-Screen-Shot.png)
+
+## **Installation**
+
+The site is available live at: https://agile-gorge-94618.herokuapp.com/. To get started, visit the website and click the "Download" icon on the URL to install on desktop or mobile devices. 
+
+## Built With
+*JavaScript *Nodejs *ExpressJs 
+*Mongoose *Morgan
+
+## **Contributors** 
+[Ulises Rosas]
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'Off-grid-finance-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
+
 const FILES_TO_CACHE = [
     './index.html',
     './css/styles.css',
@@ -17,53 +18,45 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png'
 ];
 
-
-// respond with cached resources 
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            // if cache is available, respond with cache
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request;
-            }
-            // if there is no cache available, try fetch request 
-            else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request);
-            }
-        })
-    )
-})
-
-// Cache resources 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('Installing cache : ' + CACHE_NAME)
+            console.log('installing cache: ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-})
+});
 
-// Delete outdated caches 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate' , function(e) {
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            // `keyList` contains all cache names under your username.github.io. Filter out ones that has this app prefix to create keeplist
-            let cacheKeepList = keyList.filter(function (key) {
+        caches.keys().then(function (e) {
+            let cacheKeepList = keyList.filter(function(key) {
                 return key.indexOf(APP_PREFIX);
-            })
-            // add current cache name to keeplist
+            });
             cacheKeepList.push(CACHE_NAME);
 
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeepList.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i]);
+            return Promise.all(keyList.map(function (key,i) {
+                if(cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache:' + keyList[i]);
                     return caches.delete(keyList[i]);
                 }
             }));
         })
-    );
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
+        })
+    )
 });
